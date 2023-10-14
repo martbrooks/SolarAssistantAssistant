1056f62d86aa4049f2bde484a44c1c7a0657f3a3
Martin A. Brooks <martin@hinterlands.org>
2023-10-14 09:32:24 +0100

    vars

--- SAA.pl
+++ SAA.pl
@@ -12,19 +12,25 @@ my $debug  = 1;
 my $config = LoadFile('SAA.yaml');
 my %state  = ();
 
-my $sa_mqtt_server       = $config->{sa_mqtt_server};
-my $sa_mqtt_port         = $config->{sa_mqtt_port};
-my $sa_mqtt_topic_prefix = $config->{sa_mqtt_topic_prefix};
 my $inverter_id          = 'inverter_' . $config->{inverter_id};
+my $pluge_db_password    = $config->{plunge_db_password} // '';
+my $plunge_db_host       = $config->{plunge_db_host}     // '';
+my $plunge_db_name       = $config->{plunge_db_name}     // '';
+my $plunge_db_user       = $config->{plunge_db_user}     // '';
+my $sa_mqtt_port         = $config->{sa_mqtt_port};
+my $sa_mqtt_server       = $config->{sa_mqtt_server};
+my $sa_mqtt_topic_prefix = $config->{sa_mqtt_topic_prefix};
 
 my $mqtt = Net::MQTT::Simple->new("$sa_mqtt_server:$sa_mqtt_port");
-$mqtt->subscribe( $sa_mqtt_topic_prefix . "/#", \&received );
+$mqtt->subscribe( $sa_mqtt_topic_prefix . '/#', \&received );
+$mqtt->tick();
 
 while (1) {
+    my $device_mode = $state{solar_assistant}{$inverter_id}{device_mode}{state} // '<Unknown>';
+    my $load_power  = $state{solar_assistant}{$inverter_id}{load_power}{state}  // '<Unknown>';
+    _debug("Device mode: $device_mode; Load power: $load_power");
+    sleep(5);
     $mqtt->tick();
-    sleep(10);
-    my $load_power = $state{solar_assistant}{$inverter_id}{load_power}{state} // '<Unknown>';
-    _debug("Load power: $load_power");
 }
 
 $mqtt->disconnect();
